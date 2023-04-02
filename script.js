fa290bb12fd567f19359e3efb56b47ff31cf9a21
Priyanshu Mishra <84754817+priyanshu-mishra01@users.noreply.github.com>
2023-04-02 15:34:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,105 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+const wrapper = document.querySelector(".wrapper"),
+  inputPart = document.querySelector(".input-part"),
+  infoTxt = inputPart.querySelector(".info-txt"),
+  inputField = inputPart.querySelector("input"),
+  locationBtn = inputPart.querySelector("button"),
+  weatherPart = wrapper.querySelector(".weather-part"),
+  wIcon = weatherPart.querySelector("img"),
+  arrowBack = wrapper.querySelector("header i");
 
-<head>
-  <meta charset="utf-8" />
-  <title>Weather App in JavaScript | CodingNepal</title>
-  <link rel="stylesheet" href="style.css" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <!-- Linking BoxIcon for Icon -->
-  <link href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css" rel="stylesheet" />
-</head>
+let api;
 
-<body>
-  <div class="wrapper">
-    <header><i class="bx bx-left-arrow-alt"></i>Weather App</header>
-    <section class="input-part">
-      <p class="info-txt"></p>
-      <div class="content">
-        <input type="text" spellcheck="false" placeholder="Enter city name" required />
-        <div class="separator"></div>
-        <button>Get Device Location</button>
-      </div>
-    </section>
-    <section class="weather-part">
-      <img src="" alt="Weather Icon" />
-      <div class="temp">
-        <span class="numb">_</span>
-        <span class="deg">°</span>C
-      </div>
-      <div class="weather">_ _</div>
-      <div class="location">
-        <i class="bx bx-map"></i>
-        <span>_, _</span>
-      </div>
-      <div class="bottom-details">
-        <div class="column feels">
-          <i class="bx bxs-thermometer"></i>
-          <div class="details">
-            <div class="temp">
-              <span class="numb-2">_</span>
-              <span class="deg">°</span>C
-            </div>
-            <p>Feels like</p>
-          </div>
-        </div>
-        <div class="column humidity">
-          <i class="bx bxs-droplet-half"></i>
-          <div class="details">
-            <span>_</span>
-            <p>Humidity</p>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
+inputField.addEventListener("keyup", (e) => {
+  // if user pressed enter btn and input value is not empty
+  if (e.key == "Enter" && inputField.value != "") {
+    requestApi(inputField.value);
+  }
+});
 
-  <script src="script.js"></script>
-</body>
+locationBtn.addEventListener("click", () => {
+  if (navigator.geolocation) {
+    // if browser support geolocation api
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  } else {
+    alert("Your browser not support geolocation api");
+  }
+});
 
-</html>
+function requestApi(city) {
+  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=dab3af44de7d24ae7ff86549334e45bd`;
+  fetchData();
+}
+
+function onSuccess(position) {
+  // getting lat and lon of the user device from coords obj
+  const { latitude, longitude } = position.coords;
+  api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=dab3af44de7d24ae7ff86549334e45bd`;
+  fetchData();
+}
+
+function onError(error) {
+  // if any error occur while getting user location then we'll show it in infomativeText
+  infoTxt.innerText = error.message;
+  infoTxt.classList.add("error");
+}
+
+function fetchData() {
+  infoTxt.innerText = "Getting weather details...";
+  infoTxt.classList.add("pending");
+  // getting api response and returning it with parsing into js obj and in another
+  // then function calling weatherDetails function with passing api result as an argument
+  fetch(api)
+    .then((res) => res.json())
+    .then((result) => weatherDetails(result))
+    .catch(() => {
+      infoTxt.innerText = "Something went wrong";
+      infoTxt.classList.replace("pending", "error");
+    });
+}
+
+function weatherDetails(info) {
+  if (info.cod == "404") {
+    // if user entered city name isn't valid
+    infoTxt.classList.replace("pending", "error");
+    infoTxt.innerText = `${inputField.value} isn't a valid city name`;
+  } else {
+    //getting required properties value from the whole weather information
+    const city = info.name;
+    const country = info.sys.country;
+    const { description, id } = info.weather[0];
+    const { temp, feels_like, humidity } = info.main;
+
+    // using custom weather icon according to the id which api gives to us
+    if (id == 800) {
+      wIcon.src = "icons/clear.svg";
+    } else if (id >= 200 && id <= 232) {
+      wIcon.src = "icons/storm.svg";
+    } else if (id >= 600 && id <= 622) {
+      wIcon.src = "icons/snow.svg";
+    } else if (id >= 701 && id <= 781) {
+      wIcon.src = "icons/haze.svg";
+    } else if (id >= 801 && id <= 804) {
+      wIcon.src = "icons/cloud.svg";
+    } else if ((id >= 500 && id <= 531) || (id >= 300 && id <= 321)) {
+      wIcon.src = "icons/rain.svg";
+    }
+
+    //passing a particular weather info to a particular element
+    weatherPart.querySelector(".temp .numb").innerText = Math.floor(temp);
+    weatherPart.querySelector(".weather").innerText = description;
+    weatherPart.querySelector(
+      ".location span"
+    ).innerText = `${city}, ${country}`;
+    weatherPart.querySelector(".temp .numb-2").innerText =
+      Math.floor(feels_like);
+    weatherPart.querySelector(".humidity span").innerText = `${humidity}%`;
+    infoTxt.classList.remove("pending", "error");
+    infoTxt.innerText = "";
+    inputField.value = "";
+    wrapper.classList.add("active");
+  }
+}
+
+arrowBack.addEventListener("click", () => {
+  wrapper.classList.remove("active");
+});
